723180313692e34e737928742a5381a5fd966d72
Andres Maturin <amaturin8749@sdsu.edu>
2021-03-29 19:27:41 -0700

    Program can now read the entire file.
    implemented a check to see if the end of the file is reached, when this is true the the while loop exist

--- led.cpp
+++ led.cpp
@@ -2,44 +2,51 @@
 #include <fstream> 
 #include <vector>
   using namespace std;
-  int main(int argc, char* argv[]){
-     ifstream listingFile;
-     vector<char>data; 
-      bool noNewline = true;
-      char nl;
-      
-      if(argc < 2){
-        cout<<"No listing files were provided"<< endl;  
-      return(-1);
-    }
-    
-    
-    // right now it reads until there is a newline or return
-    // what we could do is take a note at every index that there is a 13 or 10 
-    // so then we are creating the ESTAB we know where the end of the row is
-     for(int i = 2; i <= argc; i++){
-         listingFile.open(argv[i-1]);
-         // while it is true it runs
-         while(noNewline){
-           nl = listingFile.get();
-           //cout<<int(nl)<<endl;
-            //cout << "NEWLINE"<<endl;
-          if(int(nl) == 13|| int(nl) == 10){
-              //13  == carrige return
-              //10 == new line
-                noNewline = false;
-                
-          }else{
-                data.push_back(nl);
-                noNewline = true;
-          }
-         } 
-     }        
-     
-    for(int i  = 0; i < int(data.size()); i++){
-    cout << char(data[i]);
-    }
-    cout << endl;
-    return 1;
+  
 
+  int main(int argc, char* argv[]){
+	 ifstream listingFile;
+	 vector<char>data; 
+	  bool noNewline = true;
+	  char fileChar;
+	  
+	  // cheking if a listing file is provided
+	  if(argc < 2){
+		cout<<"No listing files were provided"<< endl;  
+	  return(-1);
+	}
+	
+	//opening the listing files
+	 for(int i = 2; i <= argc; i++){
+		 listingFile.open(argv[i-1]);
+	// while it is true it runs	 
+		 while(noNewline){
+		   fileChar = listingFile.get();
+//*********************************************	
+// 		this if keeps track of the newline/carriage returns
+//		what we could do is take a note at every index that there is a 13 or 10
+		//  if(int(fileChar) == 13|| int(fileChar) == 10){
+			  //13  == carrige return
+			  //10 == new line
+		//		noNewline = false;
+				
+		//  }else{
+		//		data.push_back(fileChar);
+		//		noNewline = true;
+		//  }
+//**********************************************
+	// cheking if we have reached the end of the file
+		if(listingFile.eof()){
+			noNewline = false; // ending the while loop
+		}
+		  	data.push_back(fileChar);
+		 } 
+	 }		
+
+	// printing the file
+	for(int i  = 0; i < int(data.size()-1); i++){
+	cout << char(data[i]);
+	}
+	cout << endl;
+	return 1;
   }
